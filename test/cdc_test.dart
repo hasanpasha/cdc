1da4ad30e423a9834c7cc9b10dc9406b1f52a3f9
hasanpasha <bashahsn22@gmail.com>
2025-07-08 18:41:42 +0300

    update test

--- test/cdc_test.dart
+++ test/cdc_test.dart
@@ -7,7 +7,7 @@ import 'package:test/test.dart';
 // TODO: add tests for parsing and tacky_ir and generated asm
 void main() {
   test('lexer', () {
-    expect("+-*/() 1234 main int void ++--<<>>|&^<><=>====!=!+=-=*=/=%=&=|=^=<<=>>=if else".lex().map((tok) => tok.kind), <TokenKind>[
+    expect("+-*/() 1234 main int void ++--<<>>|&^<><=>====!=!+=-=*=/=%=&=|=^=<<=>>=if else?:".lex().map((tok) => tok.kind), <TokenKind>[
       .plus,
       .hyphen,
       .asterisk,
@@ -45,6 +45,8 @@ void main() {
       .greaterGreaterEqual,
       .if$,
       .else$,
+      .questionMark,
+      .colon,
       .eoi,
     ]);
   });
